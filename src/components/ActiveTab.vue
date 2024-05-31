2c2c259ae0ccc5c5b6e9e5881b39a7a7b2be2a64
Dehya Qalbi <dehyasvibe@gmail.com>
2024-05-31 07:04:34 +0700

    active content based on active tab

--- src/components/ActiveTab.vue
+++ src/components/ActiveTab.vue
@@ -6,7 +6,8 @@
         class="cursor-pointer py-3"
         :class="isTabActive1 || isHovered1 ? 'active-tab' : ''"
         @click="activeTab1"
-        @mouseover="hoveredTab1" @mouseleave="tabNotActive1"
+        @mouseover="hoveredTab1"
+        @mouseleave="tabNotActive1"
       >
         tab 1
       </div>
@@ -14,10 +15,28 @@
         class="cursor-pointer py-3"
         :class="isTabActive2 || isHovered2 ? 'active-tab' : ''"
         @click="activeTab2"
-        @mouseover="hoveredTab2" @mouseleave="tabNotActive2"
+        @mouseover="hoveredTab2"
+        @mouseleave="tabNotActive2"
       >
         tab 2
       </div>
+    </div>
+    <div class="px-3 mt-3" v-if="isTabActive1">
+      <h2>content 1</h2>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Tenetur enim
+        ea id voluptas distinctio aut pariatur deleniti deserunt nemo velit,
+        iusto voluptatibus nesciunt soluta labore ab molestiae laudantium facere
+        perspiciatis?
+      </p>
+    </div>
+    <div class="px-3 mt-3" v-if="isTabActive2">
+      <h2>content 2</h2>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quos nihil
+        placeat magnam amet dolorem at provident, quasi saepe est, enim sit
+        ipsum odit aspernatur nam sequi quae delectus facere ducimus.
+      </p>
     </div>
   </div>
 </template>
@@ -30,16 +49,22 @@ export default {
       isTabActive2: false,
       isHovered1: false,
       isHovered2: false,
+      content1: false,
+      content2: false,
     };
   },
   methods: {
     activeTab1() {
       this.isTabActive1 = true;
       this.isTabActive2 = false;
+      this.content1 = true;
+      this.content2 = false;
     },
     activeTab2() {
       this.isTabActive2 = true;
       this.isTabActive1 = false;
+      this.content2 = true;
+      this.content1 = false;
     },
     hoveredTab1() {
       this.isHovered1 = true;
