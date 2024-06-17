156f05fad55dd7293cebbf8f5cc9d0d2a19d8269
Dehya Qalbi <dehyasvibe@gmail.com>
2024-06-17 19:00:45 +0700

    feat: props and composition API

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
-  <main>
-    <h1>Infinite Scrolling Component</h1>
-    <Suspense>
-      <InfiniteScroll />
-      <template #fallback>
-        <p>Loading...</p>
-      </template>
-    </Suspense>
-  </main>
-
+  <div class="ml-5 mt-5">
+    <input type="text" placeholder="First Name" v-model="firstName">
+    <input type="text" placeholder="Last Name" v-model="lastName">
+  </div>
+  <PersonGreeting :firstName="firstName" :lastName="lastName" />
 </template>
 
-<script setup>
-import InfiniteScroll from './components/InfiniteScroll.vue'
+<script>
+import {ref} from "vue";
+import PersonGreeting from './components/PersonGreeting.vue'
+export default {
+  components: {
+    PersonGreeting
+  },
+  setup() {
+    const firstName = ref('')
+    const lastName = ref('')
+
+    return {
+      firstName,
+      lastName
+    }
+  }
+}
 </script>
 
-<style scoped>
-h1 {
-  text-align: center;
-  margin-top: 50px;
-  margin-bottom: 20px;
-}
+<style>
 
-p {
-  text-align: center;
-}
-  
 </style>
---
+++ src/components/PersonGreeting.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="ml-5">Hello {{ fullName }}</div>
+</template>
+
+<script>
+import { computed } from 'vue'
+export default {
+    name: "PersonGreeting",
+    setup(props) {
+        const fullName = computed(()=> {
+            return `${props.firstName} ${props.lastName}`
+        })
+
+        return {
+            fullName
+        }
+    },
+    props: ['firstName', 'lastName']
+}
+</script>
+
+<style>
+
+</style>
