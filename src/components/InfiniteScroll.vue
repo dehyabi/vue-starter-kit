84775ef0cd5450ff6d524ccc11423fa7057c92ff
Dehya Qalbi <dehyasvibe@gmail.com>
2024-06-18 14:43:19 +0700

    update: add trigger onclick view more button only for first time on infinite scroll

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,25 +1,24 @@
 <template>
-    <div>
-        <ul ref="listEl" class="data" id="data">
-            <li v-for="user in usersList" :key="user.id">
-                {{user.id}} {{ user.firstName }}
-            </li>
-            <p v-if="fetchingData && nextData > 0">
-                Fetching ({{ nextData }}) more users... 
-            </p>
-            <p v-else>
-                No more data
-            </p>    
-        </ul>
-    </div>
-  
+  <div>
+    <ul ref="listEl" class="data" id="data">
+      <li v-for="user in usersList" :key="user.id">
+        {{ user.id }} {{ user.firstName }}
+      </li>
+     <div class="flex justify-center" v-if="nextData > 0">
+        <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" @click="loadMore">
+        View ({{ nextData }}) More Users
+      </button>
+     </div>
+      <p v-else class="opacity-50 flex justify-center">No more data</p>
+    </ul>
+  </div>
 </template>
 
 <script setup>
-import {ref} from "vue";
+import { ref } from "vue";
 import getUsers from "../api/getUsers";
 import getTotalUsers from "../api/getTotalUsers";
-import {useInfiniteScroll} from "@vueuse/core";
+import { useInfiniteScroll } from "@vueuse/core";
 
 const listEl = ref(null);
 
@@ -27,77 +26,66 @@ const usersToShow = 12;
 
 const usersList = ref(await getUsers(usersToShow, 0));
 
-const fetchingData = ref(null);
-
 const totalData = await getTotalUsers();
 
 const nextData = ref(totalData - usersToShow);
 
 const getUsersOnScroll = async () => {
+  await new Promise((res) => setTimeout(res, 1000));
 
+  try {
+    const newUsers = await getUsers(usersToShow, usersList.value.length);
 
-    fetchingData.value = true;
-    await new Promise((res) => setTimeout(res, 2000));
-
-    try {
-        const newUsers = await getUsers(
-        usersToShow,
-        usersList.value.length
-    );
-    fetchingData.value = null;
-    
     const currentData = usersList.value.length + usersToShow;
-    console.log('currentData: ',currentData);
+    console.log("currentData: ", currentData);
 
     const remainingData = totalData - currentData;
 
     nextData.value = remainingData;
 
     const lastData = totalData % usersToShow;
-    console.log('lastData', lastData);
+    console.log("lastData", lastData);
 
-    console.log('remainingData: ',remainingData);
+    console.log("remainingData: ", remainingData);
 
     usersList.value.push(...newUsers);
+  } catch (err) {
+    console.log(err);
+  }
+};
 
-    } catch (err) {
-        console.log(err);
-    }
-}
-
-
-
-useInfiniteScroll(
+const loadMore = () => {
+  useInfiniteScroll(
     listEl,
     async () => {
-        await getUsersOnScroll();
+      await getUsersOnScroll();
     },
     {
-        distance: 10
+      distance: 10,
     }
-);
+  );
+}
 
 </script>
 
 <style scoped>
-    div {
-        justify-content: center;
-        display: flex;
-    }
+div {
+  justify-content: center;
+  display: flex;
+}
 
-    .data {
-        background-color: rgb(136, 147, 219);
-        list-style: none;
-        max-height: 600px;
-        width: 400px;
-        overflow: scroll;
-        padding: 12px 20px;
+.data {
+  background-color: rgb(136, 147, 219);
+  list-style: none;
+  max-height: 600px;
+  width: 400px;
+  overflow: scroll;
+  padding: 12px 20px;
 
-        box-shadow: 0 20px 25px -5px rgb(0 0 0 /0.1),
-        0 8px 10px -6px rgb(0 0 0 /0.1);
-    }
+  box-shadow: 0 20px 25px -5px rgb(0 0 0 /0.1), 0 8px 10px -6px rgb(0 0 0 /0.1);
+}
 
-    li {
-        padding: 20px;
-    }
+li {
+  padding: 20px;
+}
 </style>
