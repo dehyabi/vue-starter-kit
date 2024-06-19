c825ce71d79119b2839fe0a6692fedfb5e2896f3
Dehya Qalbi <dehyasvibe@gmail.com>
2024-06-19 22:45:17 +0700

    update: scrolling window on infinite scroll

--- src/components/InfiniteScroll.vue
+++ src/components/InfiniteScroll.vue
@@ -1,14 +1,17 @@
 <template>
   <div>
-    <ul ref="listEl" class="data" id="data">
+    <ul class="data" id="data">
       <li v-for="user in usersList" :key="user.id">
         {{ user.id }} {{ user.firstName }}
       </li>
-     <div class="flex justify-center" v-if="nextData > 0">
-        <button class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600" @click="loadMore">
-        View ({{ nextData }}) More Users
-      </button>
-     </div>
+      <div class="flex justify-center" v-if="nextData > 0">
+        <button
+          class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600"
+          @click="loadMore"
+        >
+          View ({{ nextData }}) More Users
+        </button>
+      </div>
       <p v-else class="opacity-50 flex justify-center">No more data</p>
     </ul>
   </div>
@@ -20,52 +23,46 @@ import getUsers from "../api/getUsers";
 import getTotalUsers from "../api/getTotalUsers";
 import { useInfiniteScroll } from "@vueuse/core";
 
-const listEl = ref(null);
-
 const usersToShow = 12;
 
 const usersList = ref(await getUsers(usersToShow, 0));
+console.log("usersList: ", usersList);
 
 const totalData = await getTotalUsers();
 
 const nextData = ref(totalData - usersToShow);
 
-const getUsersOnScroll = async () => {
-  await new Promise((res) => setTimeout(res, 1000));
-
-  try {
-    const newUsers = await getUsers(usersToShow, usersList.value.length);
-
-    const currentData = usersList.value.length + usersToShow;
-    console.log("currentData: ", currentData);
-
-    const remainingData = totalData - currentData;
-
-    nextData.value = remainingData;
-
-    const lastData = totalData % usersToShow;
-    console.log("lastData", lastData);
-
-    console.log("remainingData: ", remainingData);
-
-    usersList.value.push(...newUsers);
-  } catch (err) {
-    console.log(err);
-  }
-};
-
 const loadMore = () => {
-  useInfiniteScroll(
-    listEl,
-    async () => {
-      await getUsersOnScroll();
-    },
-    {
-      distance: 10,
-    }
-  );
-}
+  useInfiniteScroll(window, async () => {
+    const getUsersOnScroll = async () => {
+      await new Promise((res) => setTimeout(res, 1000));
 
+      try {
+        const newUsers = await getUsers(usersToShow, usersList.value.length);
+
+        console.log("newUsers: ", newUsers);
+
+        const currentData = usersList.value.length + usersToShow;
+        console.log("currentData: ", currentData);
+
+        const remainingData = totalData - currentData;
+
+        nextData.value = remainingData;
+
+        const lastData = totalData % usersToShow;
+        console.log("lastData", lastData);
+
+        console.log("remainingData: ", remainingData);
+
+        usersList.value.push(...newUsers);
+      } catch (err) {
+        console.log(err);
+      }
+    };
+
+    await getUsersOnScroll();
+  });
+};
 </script>
 
 <style scoped>
@@ -77,8 +74,7 @@ div {
 .data {
   background-color: rgb(136, 147, 219);
   list-style: none;
-  max-height: 600px;
-  width: 400px;
+
   overflow: scroll;
   padding: 12px 20px;
 
